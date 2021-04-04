77e4df5aa3bee3be6178267e77f858f0a3480804
Sizzle50 <abdelrahmanismail789@gmail.com>
2021-04-04 14:08:20 -0700

    LSPD Dodge Challenger

---
+++ jbdchallenger/scripts/client/vehicle_names.lua
@@ -0,0 +1,18 @@
+--
+-- Copyright (C) 2018 Liara A. M. Rørvig
+--
+-- 1409 16th St S, V1C 6R5
+-- Cranbrook, British Columbia
+-- Canada
+--
+-- This file is part of "Athenas's Economy".
+--
+-- Unauthorized copying of this file, via any medium is strictly prohibited as
+-- this proprietary and confidential
+--
+
+Citizen.CreateThread(function()
+    AddTextEntry('lspd fvic', 'Crown Victoria')
+    AddTextEntry('lspd_fvictoria', 'Crown Victoria')
+    AddTextEntry('ford', 'Ford')
+end)
